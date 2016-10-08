30764cefefd69123c55c6de21e459bee5ffd319e
Dexter Barrows <dexter@barrows.io>
2016-10-08 12:00:36 -0400

    Build system now adds page titles automatically

--- stitch.sh
+++ stitch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for filenum in "$@"
-do
-    cat "$filenum"
-done
+# get page name
+pagename=$(head -1 $2 | sed 's/<[^>]*>//g')
+
+# get template top with correct name
+cat $1 | sed "s/\[TITLE\]/$pagename/"
+
+# get content
+cat $2
+
+# get footer
+cat $3
